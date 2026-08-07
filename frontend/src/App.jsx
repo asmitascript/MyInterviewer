c446d82acefc5ffc42e47066195a3e523d2e245d
Asmita Chowdhury <asmita0726@outlook.com>
2026-08-07 23:10:26 +0530

    Created main Footer

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,14 +1,18 @@
 import "./App.css";
 import Navbar from "./components/Navbar"
 import Landpage from "./pages/Landpage"
+import Footer from "./components/Footer"
 
 
 function App() {
   return (
-    <div className="background">
-      <Navbar></Navbar>
-      <Landpage></Landpage>
-    </div>
+    <>
+      <div className="background">
+        <Navbar></Navbar>
+        <Landpage></Landpage>
+      </div>
+      <Footer></Footer>
+    </>
   );
 }
 
---
+++ frontend/src/components/Footer.jsx
@@ -0,0 +1,16 @@
+import "./Footer.css"
+
+function Footer(){
+    return(
+        <div className="footer">
+            <div className="copy-right">
+                © 2026 MyInterviewer
+            </div>
+            <div className="privacy-terms">
+                Privacy | Terms
+            </div>
+        </div>
+    )
+}
+
+export default Footer;
