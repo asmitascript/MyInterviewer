21efbd2eeca21e41bc20256d9b30dcb90f35f245
Asmita Chowdhury <asmita0726@outlook.com>
2026-09-14 23:09:53 +0530

    Auth2: Login User

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -1,6 +1,6 @@
 import bcrypt from 'bcrypt';
-
-import User from "../models/user.js"
+import User from "../models/user.js";
+import jwt from 'jsonwebtoken';
 
 
 export const registerUser = async(req, res) =>{
@@ -43,4 +43,57 @@ export const registerUser = async(req, res) =>{
             message: "Registration failed",
         })
     }
+}
+
+export const loginUser = async(req, res) =>{
+
+   try{
+     const { email, password } = req.body;
+
+        const user = await User.findOne({ email });
+
+        if(!user){
+            return res.status(401).json({
+                message: "Invalid email or password",
+            });
+        }
+
+        const isPassword = await bcrypt.compare(password, user.password);
+
+        if(!isPassword){
+            return res.status(401).json({
+                message: "Invalid email or password",
+            })
+        }
+
+        // create jwt
+        const token = jwt.sign(
+            { userId: user._id },
+            process.env.JWT_SECRET,
+            { expiresIn: "7d" }
+        );
+
+        res.cookie("token", token, {
+            httpOnly: true,
+            secure: process.env.NODE_ENV === "production",
+            sameSite: "strict",
+            maxAge: 7 * 24 * 60 * 60 * 1000,
+        });
+
+        res.status(200).json({
+            message: "Login successful",
+            user: {
+                id: user._id,
+                firstName: user.firstName,
+                lastName: user.lastName,
+                email: user.email,
+            },
+        });
+    }catch(err){
+        res.status(500).json({
+            message: "Login failed",
+            error: err.message,
+        })
+    }
+
 }
--- backend/routes/authRoutes.js
+++ backend/routes/authRoutes.js
@@ -1,9 +1,11 @@
-import { registerUser } from "../controllers/authController.js";
+import { loginUser, registerUser } from "../controllers/authController.js";
 
 import express, { response } from "express";
 
 const router = express.Router();
 
 router.post("/register", registerUser);
+
+router.post("/login", loginUser);
 
 export default router;
