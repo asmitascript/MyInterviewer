47b370093739e439d31883d7f300f6dc4fadf44c
Asmita Chowdhury <asmita0726@outlook.com>
2026-09-22 11:49:04 +0530

    Auth13: Changed the session expiry to 30d

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -70,14 +70,14 @@ export const loginUser = async(req, res) =>{
         const token = jwt.sign(
             { userId: user._id },
             process.env.JWT_SECRET,
-            { expiresIn: "1m" }
+            { expiresIn: "30d" }
         );
 
         res.cookie("token", token, {
             httpOnly: true,
             secure: process.env.NODE_ENV === "production",
             sameSite: "strict",
-            maxAge: 7 * 24 * 60 * 60 * 1000,
+            maxAge: 30 * 24 * 60 * 60 * 1000,
         });
 
         res.status(200).json({
