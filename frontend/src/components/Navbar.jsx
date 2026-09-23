450ae93e8cc85626e41b3ebb0357f9723764ea15
Asmita Chowdhury <asmita0726@outlook.com>
2026-09-23 18:54:57 +0530

    UI: Show Dashboard at navbar if logged in

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,30 +1,46 @@
 import "./Navbar.css";
 
-
 import { useNavigate } from "react-router-dom";
+import { useContext } from "react";
+import { MyContext } from "../context/MyContext";
 
 function Navbar() {
-
   const navigate = useNavigate();
+  const { user } = useContext(MyContext);
+
   return (
     <nav className="navbar">
       <div className="logo">
-       <span> My</span>Interviewer
+        <span>My</span>Interviewer
       </div>
 
       <div className="nav-actions">
-        <button className="signin"
-         onClick={() => {
-          navigate("/auth/login");
-         }}
-        >Log In</button>
+        {user ? (
+          <button
+            className="signin"
+            onClick={() => navigate("/user/dashboard")}
+          >
+            Go to Dashboard<span class="material-symbols-outlined">
+              chevron_right
+            </span>
+          </button>
+        ) : (
+          <>
+            <button
+              className="signin"
+              onClick={() => navigate("/auth/login")}
+            >
+              Log In
+            </button>
 
-        <button className="get-started"
-         onClick={() => {
-          navigate("/auth/register");
-        }}>
-          Get Started
-        </button>
+            <button
+              className="get-started"
+              onClick={() => navigate("/auth/register")}
+            >
+              Get Started
+            </button>
+          </>
+        )}
       </div>
     </nav>
   );
